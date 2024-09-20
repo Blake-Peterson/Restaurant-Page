73ab0e3c31ee14c9628fa57ba34db302a9acb11d
Blake Peterson <121067208+Blake-Peterson@users.noreply.github.com>
2024-09-19 21:46:15 -0500

    added viewing images in webpack for css and html

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugins");
 
 module.exports = {
     mode: "development",
@@ -18,6 +19,14 @@ module.exports = {
           {
             test: /\.css$/i,
             use: ["style-loader", "css-loader"],
+          },
+          {
+            test: /\.html$/i,
+            loader: "html-loader",
+          },
+          {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: "asset/resource",
           },
         ],
       },
