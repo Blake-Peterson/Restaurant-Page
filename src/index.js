aa370c5520e261d8d262699415ecf606674d8ad2
Blake Peterson <121067208+Blake-Peterson@users.noreply.github.com>
2024-09-20 19:33:33 -0500

    added some additional files for later and initial js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
 import "./style.css";
 
 console.log("Hello Customer");
+
+const container = document.querySelector("#container");
+
+
+
+
+function createHomePage(){
+    createSection();
+}
+
+function createSection(){
+
+    const outerDiv  = document.createElement("div");
+    outerDiv.classList.add("outer")
+    outerDiv.style.backgroundColor="purple";
+    outerDiv.textContent = "first dummy";
+    
+    const innerDiv = document.createElement("div");
+    innerDiv.classList.add("inner");
+    innerDiv.style.backgroundColor="red";
+    innerDiv.textContent="Dummy lines";
+    outerDiv.appendChild(innerDiv);
+
+    container.appendChild(outerDiv);
+
+}
+
+
+function addReviewToHomePage(){
+
+}
+
+function addHoursToHomePage(){
+
+}
+
+function addLocationToHomePage(){
+    const locationDiv = document.createElement("div");
+    const locationh2 = document.createElement("h2");
+}
+
+createHomePage();
