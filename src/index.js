7aa639c459a33f9bf3cf450f326874a2fe1a6155
Blake Peterson <121067208+Blake-Peterson@users.noreply.github.com>
2024-09-20 19:53:37 -0500

    added weekday hours for home page

--- src/index.js
+++ src/index.js
@@ -8,21 +8,37 @@ const container = document.querySelector("#container");
 
 
 function createHomePage(){
-    createSection();
+    for(let i=0;i<3;i++){
+        switch(i){
+            case 0:
+                createSection("headline");
+                break;
+            case 1:
+                createSection("hours");
+                break;
+            case 2:
+                createSection("location");
+                break;
+        }
+    }
 }
 
-function createSection(){
+function createSection(section){
 
     const outerDiv  = document.createElement("div");
     outerDiv.classList.add("outer")
     outerDiv.style.backgroundColor="purple";
     outerDiv.textContent = "first dummy";
-    
+
     const innerDiv = document.createElement("div");
     innerDiv.classList.add("inner");
     innerDiv.style.backgroundColor="red";
     innerDiv.textContent="Dummy lines";
     outerDiv.appendChild(innerDiv);
+
+    const h2 = document.createElement("h2");
+    h2.textContent=section;
+    innerDiv.appendChild(h2);
 
     container.appendChild(outerDiv);
 
@@ -34,6 +50,40 @@ function addReviewToHomePage(){
 }
 
 function addHoursToHomePage(){
+    const HoursDiv = createSection();
+    const list = document.createElement("ul");
+
+    for(let i=0;i<7;i++){
+        const listItem = document.createElement("li");
+        switch(i){
+            //Sunday -> Saturday
+            case 0:
+                listItem.textContent= "Sunday: 8am - 8pm";
+                break;
+            case 1:
+                listItem.textContent= "Monday: 7am - 10pm";
+                break;
+            case 2:
+                listItem.textContent= "Tuesday: 7am - 10pm";
+                break;
+            case 3:
+                listItem.textContent= "Wednesday: 7am - 10pm";
+                break;
+            case 4:
+                listItem.textContent= "Thursday: 7am - 10pm";
+                break;
+            case 5:
+                listItem.textContent= "Friday: 7am - 10pm";
+                break;
+            case 6:
+                listItem.textContent= "Saturday: 7am - 10pm";
+                break;
+
+
+
+        
+        }
+    }
 
 }
 
