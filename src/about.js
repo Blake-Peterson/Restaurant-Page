b79f885459b129d0fd38bb9b634c227192b3a258
Blake Peterson <121067208+Blake-Peterson@users.noreply.github.com>
2024-09-23 15:09:15 -0500

    completed email names

--- src/about.js
+++ src/about.js
@@ -2,20 +2,23 @@ export { createAbout};
 
 function createAbout(){
     let employeeCardInfo = [
-        createAboutEmployeeCard("Bob Bossy", "Store Manager","(123) 456-7890","@skyscoops.com"),
-        createAboutEmployeeCard("Scott Scotter", "Lead Scooper","(123) 456-7891","@skyscoops.com"),
-        createAboutEmployeeCard("Jackson Weller", "Scoop Associate","(123) 456-7892","@skyscoops.com"),
+        createAboutEmployeeCard("Bob Bossy", "Store Manager","(123) 456-7890","bbossy@skyscoops.com"),
+        createAboutEmployeeCard("Scott Scotter", "Lead Scooper","(123) 456-7891","sscotter@skyscoops.com"),
+        createAboutEmployeeCard("Jackson Weller", "Scoop Associate","(123) 456-7892","jweller@skyscoops.com"),
     ];
     return employeeCardInfo; 
 }
 
 function createAboutEmployeeCard(employee_name,position,phone, email){
+    const employeeDiv = document.createElement("div");
+    employeeDiv.setAttribute("id","#employee-card");
     const nameHeader = document.createElement("h2");
     nameHeader.textContent=employee_name;
     
     for (let i=0;i<3;i++){
         const p = document.createElement("p");
         const infoDiv = document.createElement("div");
+
         switch (i){
             case 0:
                 p.textContent=position;
@@ -30,5 +33,5 @@ function createAboutEmployeeCard(employee_name,position,phone, email){
         infoDiv.appendChild(p);
         nameHeader.appendChild(infoDiv);
     }
-    return nameHeader;
+    return employeeDiv.appendChild(nameHeader);
 }
